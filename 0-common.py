53d0b9ffacf51cd758203a90ce29a1c79bc83deb
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-24 19:58:37 +0300

    Filter clean_text to contain only the most common 10,000 words

--- 0-common.py
+++ 0-common.py
@@ -22,29 +22,28 @@ startTime = time.time()
 
 # Count the most common 10,000 tokens
 counter = collections.Counter()
-fin  = open('./raw-data/text.txt', mode='r', encoding='utf-8')
-fout = open('./processed/clean_text.txt', mode='w', encoding='utf-8')
-line = fin.readline()
-while line:
-    clean_line = filter_text(line)
-    fout.write(clean_line)
-    tokens = clean_line.replace('\n', ' ').split()
-    tokens = [t for t in tokens if t.strip() != '']
-    counter.update(tokens)
-    line = fin.readline()
-fin.close()
-fout.close()
+clean_text = ''
+with open('./raw-data/text.txt', mode='r', encoding='utf-8') as f:
+    line = f.readline()
+    while line:
+        tokens = filter_text(line).replace('\n', ' ').split()
+        tokens = [t for t in tokens if t.strip() != '']
+        clean_text += ' '.join(tokens)
+        counter.update(tokens)
+        line = f.readline()
 
-# pickle.dump(counter, open('words_counter.pkl', 'wb'))
+# Get only the most common 10,000
+most_common = list()
+for word, count in counter.most_common(10000):
+    most_common.append(word)
 
-# most_common = counter.most_common(10000)
-# f1 = open('10000_most_common_words.txt', 'w', encoding='utf-8')
-# f2 = open('10000_most_common_words_WORDS_ONLY.txt', 'w', encoding='utf-8')
-# for word, count in most_common:
-#     f1.write('{}\t{}\n'.format(word, count))
-#     f2.write('{}\n'.format(word))
-# f1.close()
-# f2.close()
+del counter
+clean_tokens = [t for t in clean_text.split() if t in most_common]
+
+# Write them to clean_text.txt
+with open('./processed/clean_text.txt', mode='w', encoding='utf-8') as f:   
+    for token in clean_tokens:
+        f.write(token + ' ')
 
 # Display elapsed time
 endTime = time.time()

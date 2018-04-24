1b427e3bcf6de0c1e5f508b69bdd763289da5478
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-24 19:59:17 +0300

    Filepath instead of a whole directory of files.

--- 1-word2vec-gensim.py
+++ 1-word2vec-gensim.py
@@ -4,15 +4,15 @@ import gensim
 import os
 
 class MySentences(object):
-    def __init__(self, dirname):
-        self.dirname = dirname
+    def __init__(self, filepath):
+        self.filepath = filepath
  
     def __iter__(self):
-        for fname in os.listdir(self.dirname):
-            for line in open(os.path.join(self.dirname, fname), encoding='utf-8'):
-                yield line.split()
+        # for fname in os.listdir(self.dirname):
+        for line in open(self.filepath, encoding='utf-8'):
+            yield line.split()
  
-sentences = MySentences('./processed') # a memory-friendly iterator
+sentences = MySentences('./processed/clean_text.txt') # a memory-friendly iterator
 model = gensim.models.Word2Vec(sentences=sentences, size=100, window=5, max_vocab_size=10000,  min_count=5, workers=4)
 model.save('./results/gensim.model')
 

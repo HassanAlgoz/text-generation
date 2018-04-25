14dab762dab43f533016258dc3790682eb0ec85c
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-25 14:59:32 +0300

    Added the missing tokenization step.

--- 2-preprocess.py
+++ 2-preprocess.py
@@ -1,5 +1,7 @@
 import re
 import string
+from keras.preprocessing.text import Tokenizer
+import pickle
  
 # load text
 with open('./processed/clean_text.txt', encoding='utf-8') as f:
@@ -10,6 +12,13 @@ tokens = text.split()
 print(tokens[:200])
 print('Total Tokens: %d' % len(tokens))
 print('Unique Tokens: %d' % len(set(tokens)))
+
+# Tokenizer
+tokenizer = Tokenizer(filters="", oov_token="UNK") # we don't filter any characters, we need commans full stops and everything.
+tokenizer.fit_on_texts(tokens)
+# save the tokenizer
+pickle.dump(tokenizer, open('./processed/tokenizer.pkl', 'wb'))
+del tokenizer
  
 # organize into sequences of tokens
 length = 2 + 1

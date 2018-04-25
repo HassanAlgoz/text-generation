267976eae3544fcb6e2aa20052c1ca917df8b7af
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-25 14:34:26 +0300

    Phrases detection added.

--- 1-word2vec-gensim.py
+++ 1-word2vec-gensim.py
@@ -1,9 +1,9 @@
 import warnings
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-import gensim
+from gensim.models import Word2Vec, Phrases
 import os
 
-class MySentences(object):
+class SentenceGenerator(object):
     def __init__(self, filepath):
         self.filepath = filepath
  
@@ -12,9 +12,17 @@ class MySentences(object):
         for line in open(self.filepath, encoding='utf-8'):
             yield line.split()
  
-sentences = MySentences('./processed/clean_text.txt') # a memory-friendly iterator
-model = gensim.models.Word2Vec(sentences=sentences, size=100, window=5, max_vocab_size=10000,  min_count=5, workers=4)
+sentences = SentenceGenerator('./processed/clean_text.txt') # a memory-friendly iterator
+
+# Note that there is a gensim.models.phrases module which lets you automatically detect 
+# phrases longer than one word. Using phrases, you can learn a word2vec model where “words”
+#  are actually multiword expressions, such as new_york_times or financial_crisis:
+bigram_transformer = Phrases(sentences)
+model = Word2Vec(bigram_transformer[sentences], size=100, window=5, max_vocab_size=10000,  min_count=5, workers=4)
+
+# Save the model
 model.save('./results/gensim.model')
 
+# model.wv.save_word2vec_format('./results/gensim.wv')
 # m2 = gensim.models.Word2Vec.load('./results/gensim.model')
 # print(m2.wv["قال"])

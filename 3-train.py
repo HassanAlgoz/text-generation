b20416ec9b81dfd7d1ddc26dba2d1111535dd73c
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-26 14:26:47 +0300

    Training now will resume if a saved model exists.

--- 3-train.py
+++ 3-train.py
@@ -4,6 +4,7 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential
+from keras.models import load_model
 from keras import layers
 from keras import callbacks
 import warnings
@@ -11,12 +12,17 @@ warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 import gensim
 import os
 
+PATH_SEQUENCES    = './processed/sequences.txt'
+PATH_TOKENIZER    = './processed/tokenizer.pkl'
+PATH_GENSIM_MODEL = './results/gensim.model'
+PATH_MODEL        = './results/model.h5'
+
 # load sequences
-with open('./processed/sequences.txt', encoding='utf-8') as f:
+with open(PATH_SEQUENCES, encoding='utf-8') as f:
 	texts = f.read().split('\n')
 
 # Load tokenizer and convert word sequences to their integers
-tokenizer = pickle.load(open('./processed/tokenizer.pkl', 'rb'))
+tokenizer = pickle.load(open(PATH_TOKENIZER, 'rb'))
 sequences = np.array(tokenizer.texts_to_sequences(texts))
 vocab_size = len(tokenizer.word_index) + 1
 print(sequences[:5])
@@ -24,7 +30,7 @@ word_index = tokenizer.word_index
 del tokenizer
 
 # Embeddings
-word2vec = gensim.models.Word2Vec.load('./results/gensim.model')
+word2vec = gensim.models.Word2Vec.load(PATH_GENSIM_MODEL)
 word_vectors = word2vec.wv
 vector_size = word_vectors.vector_size
 del word2vec
@@ -45,26 +51,32 @@ y = to_categorical(y, num_classes=vocab_size)
 seq_length = X.shape[1]
 
 # Define model
-model = Sequential([
-    layers.Embedding(
-        input_dim=vocab_size,
-        output_dim=vector_size,
-        input_length=seq_length,
-        trainable=False,
-        weights=[embedding_matrix]
-    ),
-    layers.GRU(vector_size),
-    layers.Dense(vocab_size, activation='softmax'),
-])
+if os.path.exists(PATH_MODEL):
+    print("loading a saved model...")
+    model = load_model(PATH_MODEL)
+else:
+    print("starting from a new model...")
+    model = Sequential([
+        layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=vector_size,
+            input_length=seq_length,
+            trainable=False,
+            weights=[embedding_matrix]
+        ),
+        layers.GRU(vector_size),
+        layers.Dense(vocab_size, activation='softmax'),
+    ])
+    # compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
+
 model.summary()
-# compile model
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
 
 # save model after each epoch
-checkpoint_cb = callbacks.ModelCheckpoint('./results/model.h5')
+checkpoint_cb = callbacks.ModelCheckpoint(PATH_MODEL)
 # if the value monitored doesn't improve in 10 epochs, stop training.
 earlystop_cb = callbacks.EarlyStopping(monitor='loss', patience=10, mode='min')
-
 # fit model
-model.fit(X, y, batch_size=100, epochs=5, verbose=2, callbacks=[checkpoint_cb, earlystop_cb])
+model.fit(X, y, batch_size=10, epochs=1000, verbose=2, callbacks=[checkpoint_cb, earlystop_cb])
 

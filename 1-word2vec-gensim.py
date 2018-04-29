582b907712bc06084d1f7d7f55a881c8b69f870c
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-29 10:40:57 +0300

    Gensim Word2Vec training added. It turns out, it wasn't training.

--- 1-word2vec-gensim.py
+++ 1-word2vec-gensim.py
@@ -4,6 +4,9 @@ from gensim.models import Word2Vec, Phrases
 import os
 import helper.paths as PATH
 import helper.args as args
+from helper.tick import Tick
+
+tick = Tick()
 
 class SentenceGenerator(object):
     def __init__(self, filepath):
@@ -16,15 +19,31 @@ class SentenceGenerator(object):
  
 sentences = SentenceGenerator(PATH.CLEAN_TEXT) # a memory-friendly iterator
 
+num_words = 0
+with open(PATH.CLEAN_TEXT, encoding='utf-8') as f:
+    for line in f:
+        num_words += len(line.split())
+
+
+# http://kavita-ganesan.com/gensim-word2vec-tutorial-starter-code/#.WuT-OYhuaiM
 # Note that there is a gensim.models.phrases module which lets you automatically detect 
 # phrases longer than one word. Using phrases, you can learn a word2vec model where “words”
 #  are actually multiword expressions, such as new_york_times or financial_crisis:
+tick_1 = Tick("Building Word2Vec")
 bigram_transformer = Phrases(sentences)
-model = Word2Vec(bigram_transformer[sentences], size=args.EMBEDDING_HIDDEN_SIZE, window=args.EMBEDDING_WINDOW_SIZE, max_vocab_size=args.MAX_VOCAB_SIZE,  min_count=5, workers=4)
+model = Word2Vec(bigram_transformer[sentences], size=args.EMBEDDING_HIDDEN_SIZE, window=args.EMBEDDING_WINDOW_SIZE, max_vocab_size=args.MAX_VOCAB_SIZE,  min_count=5, workers=os.cpu_count())
+tick_1.tock()
+
+tick_2 = Tick("Gensim Training")
+model.train(bigram_transformer[sentences], total_words=num_words, epochs=args.EMBEDDING_EPOCHS)
+tick_2.tock()
 
 # Save the model
 model.save(PATH.GENSIM_MODEL)
 
+
 # model.wv.save_word2vec_format('./results/gensim.wv')
 # m2 = gensim.models.Word2Vec.load('./results/gensim.model')
 # print(m2.wv["قال"])
+
+tick.tock()

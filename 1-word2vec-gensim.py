7ff9857937a6c40375edb08005942d742bc0ec1a
Hassan Algoz <HassanAlgoz@gmail.com>
2018-04-30 17:05:55 +0300

    Phrases (Fivegram) added to the gensim word2vec model.

--- 1-word2vec-gensim.py
+++ 1-word2vec-gensim.py
@@ -25,26 +25,31 @@ with open(PATH.CLEAN_TEXT, encoding='utf-8') as f:
     for line in f:
         num_words += len(line.split())
 
-
 # http://kavita-ganesan.com/gensim-word2vec-tutorial-starter-code/#.WuT-OYhuaiM
 # Note that there is a gensim.models.phrases module which lets you automatically detect 
 # phrases longer than one word. Using phrases, you can learn a word2vec model where “words”
 #  are actually multiword expressions, such as new_york_times or financial_crisis:
-ticker.tick("Building Word2Vec")
-bigram_transformer = Phrases(sentences)
-model = Word2Vec(bigram_transformer[sentences], size=args.EMBEDDING_HIDDEN_SIZE, window=args.EMBEDDING_WINDOW_SIZE, max_vocab_size=args.MAX_VOCAB_SIZE,  min_count=5, workers=os.cpu_count())
+
+ticker.tick("five-gram phrases model")
+bigram = Phrases(sentences)
+trigram = Phrases(bigram[sentences])
+quadgram = Phrases(trigram[bigram[sentences]])
+fivegram = Phrases(quadgram[trigram[bigram[sentences]]])
 ticker.tock()
 
+if os.path.exists(PATH.GENSIM_MODEL):
+    print("load an existing gensim model...")
+    model = Word2Vec.load(PATH.GENSIM_MODEL)
+else:
+    ticker.tick("Building Word2Vec")
+    model = Word2Vec(fivegram[quadgram[trigram[bigram[sentences]]]], size=args.EMBEDDING_HIDDEN_SIZE, window=args.EMBEDDING_WINDOW_SIZE, max_vocab_size=args.MAX_VOCAB_SIZE,  min_count=5, workers=os.cpu_count())
+    ticker.tock()
+
 ticker.tick("Gensim Training")
-model.train(bigram_transformer[sentences], total_words=num_words, epochs=args.EMBEDDING_EPOCHS)
+model.train(fivegram[quadgram[trigram[bigram[sentences]]]], total_words=num_words, epochs=args.EMBEDDING_EPOCHS)
 ticker.tock()
 
 # Save the model
 model.save(PATH.GENSIM_MODEL)
-
-
-# model.wv.save_word2vec_format('./results/gensim.wv')
-# m2 = gensim.models.Word2Vec.load('./results/gensim.model')
-# print(m2.wv["قال"])
 
 ticker.tock()
